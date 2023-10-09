da793ec1eb038ee72ab5609fd06fa7ffa2e6fbc2
Neeraj Rai <neeraj2000rai@gmail.com>
2023-10-09 20:14:09 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, //5 mins
+      staleTime: 1000 * 60 * 5, //5 mins(time until a query result is cached)
     },
   },
 });
